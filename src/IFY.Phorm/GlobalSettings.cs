aac25bf3b93147da52ff1276a7ced2ae13e76f6f
Ian Yates <ian.yates@neofintechlab.com>
2022-03-15 09:16:39 +0000

    Expose global JSON settings property

--- src/IFY.Phorm/GlobalSettings.cs
+++ src/IFY.Phorm/GlobalSettings.cs
@@ -1,4 +1,5 @@
 ﻿using IFY.Phorm.Encryption;
+using Newtonsoft.Json;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("IFY.Phorm.Tests")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("IFY.Phorm.SqlClient.Tests")]
@@ -8,6 +9,12 @@ namespace IFY.Phorm
 {
     public static class GlobalSettings
     {
+        /// <summary>
+        /// The global <see cref="JsonSerializerSettings"/> to use when dealing with JSON.
+        /// If null, uses library defaults.
+        /// </summary>
+        public static JsonSerializerSettings? NewtonsoftJsonSerializerSettings { get; set; }
+
         /// <summary>
         /// The registered instance of the <see cref="IEncryptionProvider"/> to use for secure value handling.
         /// </summary>
