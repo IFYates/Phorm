238e2874230eb23eaaf0f6072e0a5e32edf9ce69
IFYates <ian@iyates.co.uk>
2022-11-23 19:05:41 +0000

    Better SecureValueAttribute definition

--- src/IFY.Phorm/Encryption/SecureValueAttribute.cs
+++ src/IFY.Phorm/Encryption/SecureValueAttribute.cs
@@ -7,8 +7,14 @@ namespace IFY.Phorm.Encryption;
 /// </summary>
 public class SecureValueAttribute : AbstractSecureValueAttribute
 {
-    private readonly string _dataClassification;
-    private readonly string? _authenticatorPropertyName;
+    /// <summary>
+    /// The identifier for the classification of the data, used to determine the encryption/decryption implementation.
+    /// </summary>
+    public string DataClassification { get; }
+    /// <summary>
+    /// The name of a property on this contract/DTO that is used as an encryption/decryption authenticator.
+    /// </summary>
+    public string? AuthenticatorPropertyName { get; }
 
     private static object? _lastInstance;
     private static string? _lastProperty;
@@ -20,7 +26,7 @@ public class SecureValueAttribute : AbstractSecureValueAttribute
     /// <param name="dataClassification">The classification for this value type, which can be used to control the encryption used.</param>
     public SecureValueAttribute(string dataClassification)
     {
-        _dataClassification = dataClassification;
+        DataClassification = dataClassification;
     }
     /// <summary>
     /// This contract property represents a value that is stored encrypted.
@@ -29,8 +35,8 @@ public class SecureValueAttribute : AbstractSecureValueAttribute
     /// <param name="authenticatorPropertyName">The name of the property holding the authenticator value.</param>
     public SecureValueAttribute(string dataClassification, string? authenticatorPropertyName)
     {
-        _dataClassification = dataClassification;
-        _authenticatorPropertyName = authenticatorPropertyName;
+        DataClassification = dataClassification;
+        AuthenticatorPropertyName = authenticatorPropertyName;
     }
 
     private static byte[] resolveAuthenticator(object? context, string? propertyName)
@@ -56,6 +62,7 @@ public class SecureValueAttribute : AbstractSecureValueAttribute
         return _lastValue;
     }
 
+    /// <inheritdoc/>
     public override byte[]? Decrypt(byte[]? value, object? context)
     {
         if (value == null)
@@ -68,17 +75,18 @@ public class SecureValueAttribute : AbstractSecureValueAttribute
             throw new InvalidOperationException($"The {nameof(GlobalSettings.EncryptionProvider)} has not not been registered.");
         }
 
-        var encryptor = GlobalSettings.EncryptionProvider.GetDecryptor(_dataClassification, value);
+        var encryptor = GlobalSettings.EncryptionProvider.GetDecryptor(DataClassification, value);
         if (encryptor == null)
         {
             // TODO: Fail instead?
             return value;
         }
 
-        encryptor.Authenticator = resolveAuthenticator(context, _authenticatorPropertyName);
+        encryptor.Authenticator = resolveAuthenticator(context, AuthenticatorPropertyName);
         return encryptor.Decrypt(value);
     }
 
+    /// <inheritdoc/>
     public override byte[] Encrypt(object? value, object? context)
     {
         if (value == null)
@@ -92,14 +100,14 @@ public class SecureValueAttribute : AbstractSecureValueAttribute
             throw new InvalidOperationException($"The {nameof(GlobalSettings.EncryptionProvider)} has not not been registered.");
         }
 
-        var encryptor = GlobalSettings.EncryptionProvider.GetEncryptor(_dataClassification);
+        var encryptor = GlobalSettings.EncryptionProvider.GetEncryptor(DataClassification);
         if (encryptor == null)
         {
             // TODO: Fail instead?
             return bytes;
         }
 
-        encryptor.Authenticator = resolveAuthenticator(context, _authenticatorPropertyName);
+        encryptor.Authenticator = resolveAuthenticator(context, AuthenticatorPropertyName);
         return encryptor.Encrypt(bytes);
     }
 }
